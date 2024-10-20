60e4e29c80d15d4f368f3f1dfac1d6b12b9521e7
Hofta1 <c.collhins@gmail.com>
2024-10-20 22:32:55 +0700

    Testing recycler view dengan api 18/10/2024 (jalan)
    Rapihin Listing page 20/10/2024 17:10
    
    Top part details page 20/10/2024 22:32

--- app/src/main/java/com/example/mobileexercise_99/MainActivity.kt
+++ app/src/main/java/com/example/mobileexercise_99/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileexercise_99.data.model.listing.Listing
@@ -21,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         recyclerView = findViewById(R.id.listingRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         viewModel.getData()
